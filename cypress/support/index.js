4ec92825ba072f31ed00536cfcee28324715d311
Arek <arkadiusz.pecherz@rst-it.com>
2019-11-19 14:48:58 +0100

    new line adding, removing unnecessary import

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
 import './auth'
 
 // Alternatively you can use CommonJS syntax:
