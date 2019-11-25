6f0105f386d2d334f864213fe4a904fd54882620
Arek <arkadiusz.pecherz@rst-it.com>
2019-11-25 11:00:29 +0100

    eslint fixes

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './auth'
+import './auth';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
