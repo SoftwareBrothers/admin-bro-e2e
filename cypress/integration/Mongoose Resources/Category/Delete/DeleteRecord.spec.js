1fc71a3e58f81338f0cc5d3a9b911ec2e38bd45e
Wojtek Nowacki <wojciech.nowacki@softwarebrothers.co>
2019-11-27 12:45:32 +0100

    table name fixed

--- cypress/integration/Mongoose Resources/Category/Delete/DeleteRecord.spec.js
+++ cypress/integration/Mongoose Resources/Category/Delete/DeleteRecord.spec.js
@@ -14,14 +14,14 @@ describe('Delete record',function(){
   it('From record details page and clicking remove button',function(){
     cy.loginSuccess() 
       .get(leftNavbar.mongoose.category).contains(navbarTexts.mongoose.category).click()
-      .get(boardView.table).then($table=>{
-        const idofFirstRecord = $table.find(boardView.tableTds).eq(1);
+      .get(boardView.table).then($tableWithRecords=>{
+        const idofFirstRecord = $tableWithRecords.find(boardView.tableTds).eq(1);
         cy.wrap(idofFirstRecord).as('firstRecordId');
       });
     cy.get(boardView.tableTds).first().find('a').click()
       .get(buttons.remove).click()
-      .get(boardView.table).then($table=>{ 
-        const firstRecordIdAfeterDelete = $table.find(boardView.tableTds).eq(1).text();
+      .get(boardView.table).then($tableWithRecords=>{ 
+        const firstRecordIdAfeterDelete = $tableWithRecords.find(boardView.tableTds).eq(1).text();
         expect(this.firstRecordId.text()).not.to.equal(firstRecordIdAfeterDelete);
       });
   });
