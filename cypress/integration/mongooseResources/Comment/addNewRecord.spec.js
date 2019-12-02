b957d220adc1c2f8e5136222c0933b30269794a5
Wojtek Nowacki <wojciech.nowacki@softwarebrothers.co>
2019-12-02 07:35:13 +0100

    template done, refator needed

--- cypress/integration/mongooseResources/Comment/addNewRecord.spec.js
+++ cypress/integration/mongooseResources/Comment/addNewRecord.spec.js
@@ -1,27 +1,26 @@
 import {
   mongoose,
   leftNavbar,
-} from '../../../../support/cssCommonSelectors';
+} from '../../../support/cssCommonSelectors';
 import {
   common,
   navbarTexts,
-} from '../../../../support/texts';
+} from '../../../support/texts';
 
 const { inputs, calendar, buttons, validationFields } = mongoose;
 const { inputsTexts,buttons: textButtons, errorMsg } = common;
 
-describe('', function(){
-  it('', function() {
+describe('Adding new record to comment', function(){
+  it('New comment by click on add new button', function() {
     cy.loginSuccess() 
       .get(leftNavbar.mongoose.comment).contains(navbarTexts.mongoose.comment).click()
-      .get(buttons.addNew).contains(textButtons.addNew).click()
-      .get(inputs.nestedValue).type(inputsTexts.randomNumbers)
-      .get(inputs.nestedFiled).type(common.randomText)
-      .get(inputs.owner).type(inputsTexts.ownerRandom)
-      .get(inputs.createdAt).click()
-      .get(calendar.todayClass).eq(2).click() 
-      .get(buttons.save).contains(common.save).click() 
-      .get(validationFields.validationInput).contains(errorMsg.titleField)
-      .get(validationFields.validationDiv).should('be.visible').contains(errorMsg.validationDiv);
+      .get('.icomoon-add').click()
+      .get('.css-1wy0on6').first().click()
+      .wait(3000)
+      .get('div').contains('Baby').first().click()
+      .get('#content').type(inputsTexts.randomNumbers)
+      .get('#flagged').click()
+      .get(buttons.save).contains(common.save).click(); 
+
   });
 });
