74e1007ac922d1d14b35eb9f1249fcf2aec2ccff
Wojtek Nowacki <wojciech.nowacki@softwarebrothers.co>
2019-12-02 08:31:25 +0100

    temp

--- cypress/integration/mongooseResources/Comment/addNewRecord.spec.js
+++ cypress/integration/mongooseResources/Comment/addNewRecord.spec.js
@@ -15,12 +15,18 @@ describe('Adding new record to comment', function(){
     cy.loginSuccess() 
       .get(leftNavbar.mongoose.comment).contains(navbarTexts.mongoose.comment).click()
       .get('.icomoon-add').click()
-      .get('.css-1wy0on6').first().click()
-      .wait(3000)
-      .get('div').contains('Baby').first().click()
-      .get('#content').type(inputsTexts.randomNumbers)
+      .get('.css-1wy0on6').first().click().then($dropdown=>{
+        const category =  $dropdown.find('.css-11unzgr').find('[tabindex="-1"]').first().text();
+        cy.wrap(category).as('categoryName'); 
+      }) 
+      .get('.css-11unzgr').find('[tabindex="-1"]').first().click() 
+      .get('#content').type(inputsTexts.randomNumbers) 
       .get('#flagged').click()
-      .get(buttons.save).contains(common.save).click(); 
+      .get(buttons.save).contains(common.save).click()
+      .wait(3000)
+      .get(buttons.back).click()
+      .get('a[href="/?direction=desc&sortBy=_id"]').click();
+      
 
   });
 });
