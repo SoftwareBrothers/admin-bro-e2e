956ed5052d71fe788bd8b8cb92bbb094805cfe1c
Wojtek Nowacki <wojciech.nowacki@softwarebrothers.co>
2019-12-03 08:23:36 +0100

    finished task

--- cypress/integration/mongooseResources/Comment/addNewRecord.spec.js
+++ cypress/integration/mongooseResources/Comment/addNewRecord.spec.js
@@ -7,26 +7,36 @@ import {
   navbarTexts,
 } from '../../../support/texts';
 
-const { inputs, calendar, buttons, validationFields } = mongoose;
-const { inputsTexts,buttons: textButtons, errorMsg } = common;
+const { inputs, calendar, buttons, boardView, boardView:{ sorting } } = mongoose;
+const { inputsTexts } = common;
 
 describe('Adding new record to comment', function(){
   it('New comment by click on add new button', function() {
+
     cy.loginSuccess() 
       .get(leftNavbar.mongoose.comment).contains(navbarTexts.mongoose.comment).click()
-      .get('.icomoon-add').click()
-      .get('.css-1wy0on6').first().click().then($dropdown=>{
-        const category =  $dropdown.find('.css-11unzgr').find('[tabindex="-1"]').first().text();
-        cy.wrap(category).as('categoryName'); 
-      }) 
-      .get('.css-11unzgr').find('[tabindex="-1"]').first().click() 
-      .get('#content').type(inputsTexts.randomNumbers) 
-      .get('#flagged').click()
+      .get(buttons.addIcon).click()
+      .get(buttons.dropDownButton).first().click()
+      .get(buttons.dropDownOption).then($element=>{
+        const category = $element.text();
+        cy.wrap(category).as('category');
+      });
+    cy.get(buttons.dropDownOption).first().click() 
+      .get(inputs.content).type(inputsTexts.randomNumbers) 
+      .get(inputs.checkBoxFlagged).click() 
       .get(buttons.save).contains(common.save).click()
-      .wait(3000)
-      .get(buttons.back).click()
-      .get('a[href="/?direction=desc&sortBy=_id"]').click();
-      
-
+      .wait(1000)
+      .get(buttons.back).click() 
+      .get(sorting.sortByIdButton).click() 
+      .wait(1000)
+      .get(boardView.table).then($tableWithRecords=>{ 
+        const categoryObBoard = $tableWithRecords.find(boardView.tableTdClass).eq(1).text();
+        const flaggedOnBoard = $tableWithRecords.find(boardView.tableTdClass).eq(2).text();
+        const contentOnBoard = $tableWithRecords.find(boardView.tableTdClass).eq(3).text();
+  
+        expect(this.category).to.be.equal(categoryObBoard);
+        expect(flaggedOnBoard).to.be.equal('Yes');
+        expect(contentOnBoard).to.be.equal(inputsTexts.randomNumbers);
+      });
   });
 });
