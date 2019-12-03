f66e66e8d8226e54bb29435fd093ff86d67acd1c
Wojtek Nowacki <wojciech.nowacki@softwarebrothers.co>
2019-12-03 14:49:20 +0100

    deleting record done

--- cypress/integration/mongooseResources/Comment/delete/deleteRecords.spec.js
+++ cypress/integration/mongooseResources/Comment/delete/deleteRecords.spec.js
@@ -3,25 +3,23 @@ import {
   leftNavbar,
 } from '../../../../support/cssCommonSelectors';
 import {
-  common,
   navbarTexts,
 } from '../../../../support/texts';
-import { clearScreenDown } from 'readline';
 
 const { boardView, buttons } = mongoose;
 
-describe('Delete category record',function(){
+describe('Delete comment record',function(){
   it('From record details page and clicking remove button',function(){
     cy.loginSuccess() 
-      .get(leftNavbar.mongoose.category).contains(navbarTexts.mongoose.category).click()
+      .get(leftNavbar.mongoose.comment).contains(navbarTexts.mongoose.comment).click()
       .get(boardView.table).then($tableWithRecords=>{
-        const idofFirstRecord = $tableWithRecords.find(boardView.tableTds).eq(1);
-        cy.wrap(idofFirstRecord).as('firstRecordId');
+        const idOfFirstRecord = $tableWithRecords.find(boardView.tableTds).eq(0);
+        cy.wrap(idOfFirstRecord).as('firstRecordId');
       });
-    cy.get(boardView.tableTds).first().find('a').click()
+    cy.get(boardView.tableTds).eq(3).find('a').click()
       .get(buttons.remove).click()
       .get(boardView.table).then($tableWithRecords=>{ 
-        const firstRecordIdAfeterDelete = $tableWithRecords.find(boardView.tableTds).eq(1).text();
+        const firstRecordIdAfeterDelete = $tableWithRecords.find(boardView.tableTds).eq(0).text();
         expect(this.firstRecordId.text()).not.to.equal(firstRecordIdAfeterDelete);
       });
   });
