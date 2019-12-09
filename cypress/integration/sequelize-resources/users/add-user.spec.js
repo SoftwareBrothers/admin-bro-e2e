7094abca6439e7d26fdb8bf3d757e164fc1c39ed
areksb <38556503+areksb@users.noreply.github.com>
2019-12-09 14:36:28 +0100

    Sequelize resources (#10)
    Sequelize resources:
    - add user
    - add place

---
+++ cypress/integration/sequelize-resources/users/add-user.spec.js
@@ -0,0 +1,20 @@
+import { sequelize, leftNavbar } from '../../../support/cssCommonSelectors';
+import { common } from '../../../support/texts';
+
+describe('Add new user to Sequelize Resources', () => {
+  it('Create a new user', () => {
+    cy.loginSuccess()
+      .get(leftNavbar.sequelize.users).click()
+      .get('.btn-text').contains('Add new').click()
+      .url().should('contain', '/admin/resources/Users/actions/new')
+      .get(sequelize.inputs.email).type('email@example.com')
+      .get(sequelize.inputs.isMyFavourite).type('true') //here typing not-boolean causes 500
+      .get(sequelize.inputs.lastName).type('Lastname')
+      .get(sequelize.inputs.firstName).type('Firstname')
+      .get(sequelize.inputs.gender).next().click()
+      .get(sequelize.inputs.male).click()
+      .get('.btn-text').contains('Save').click()
+      .get('.success').should('be.visible')
+      .get('.success').should('contain', common.recordCreated);
+  });
+});
