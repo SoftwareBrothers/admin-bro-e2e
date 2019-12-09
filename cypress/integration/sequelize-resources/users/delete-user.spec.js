2c88fca335ef99d9518acf17f648c453a7a16b8c
Arek <arkadiusz.pecherz@rst-it.com>
2019-12-09 13:25:42 +0100

    user remove

---
+++ cypress/integration/sequelize-resources/users/delete-user.spec.js
@@ -0,0 +1,17 @@
+import { leftNavbar, mongoose } from '../../../support/cssCommonSelectors';
+import { common } from '../../../support/texts';
+
+describe('Delete added user from Sequelize Resources', () => {
+  it('Enter users, delete created one', () => {
+    cy.loginSuccess()
+      .get(leftNavbar.sequelize.users).click()
+      .get('.is-sortable').contains('Created At').click()
+      .wait(500)
+      .get('.is-sortable').contains('Created At').click()
+      .wait(500)
+      .get('.main').contains('email@example.com').click()
+      .get(mongoose.buttons.remove).click()
+      .get('.success').should('be.visible')
+      .get('.success').should('contain', common.recordDeleted);
+  });
+});
