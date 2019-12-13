ba636435508054b2b6b6140527b398bf90a36579
Wojtek Nowacki <wojciech.nowacki@softwarebrothers.co>
2019-12-13 11:53:52 +0100

    finished adding recs for complicated

--- cypress/integration/mongooseResources/complicated/add/addNewRecord.spec.js
+++ cypress/integration/mongooseResources/complicated/add/addNewRecord.spec.js
@@ -7,7 +7,7 @@ import {
   common,
   navbarTexts,
 } from '../../../../support/texts';
-import { getFormValues,
+import { getTextFromChildElements,
 } from '../../../../support/helpersMethods';
 
 const { inputs, calendar, buttons, boardView } = mongoose;
@@ -16,25 +16,29 @@ const { inputsTexts  } = common;
 describe('Add record to the complicated', () => {
   it('Create record with basic info only', () => {
     cy.loginSuccess()
-      .get(leftNavbar.mongoose.complicated).contains(navbarTexts.mongoose.category).click()
+      .get(leftNavbar.mongoose.complicated).contains(navbarTexts.mongoose.complicated).click()
       .get(buttons.addIcon).click()
-      .get(inputs.name).type(inputsTexts.title)
-      .get(inputs.personAge).type(inputsTexts.randomNumbers)
-      .get(inputs.height).type(common.randomText)
-      .get(inputs.birthPlace).type(inputsTexts.ownerRandom)
-      .get(inputs.extremlyNested).click()
+      .get(inputs.name).type(inputsTexts.name) 
+      .get(inputs.personAge).type(inputsTexts.personAge)
+      .get(inputs.height).type(inputsTexts.height)
+      .get(inputs.birthPlace).type(inputsTexts.birthPlace)
+      .get(inputs.extremlyNested).type(inputsTexts.extremlyNested)
       .get(buttons.save).contains(common.save).click()
       .wait(1000)
       .get(buttons.back).click()
       .get(boardView.table).find(boardView.tableTr).eq(1).then($tr=>{ 
-        const finputValues = getFormValues($tr, [0,2,3,4]);
-        expect(finputValues) 
-          .to.have.members([
-            inputsTexts.randomNumbers, 
-            common.randomText,
-            inputsTexts.ownerRandom,
-            inputsTexts.title,
-          ]);
+        const name = $tr.find(boardView.tableTdClass).eq(0);
+        expect(name.text()).to.be.eql(inputsTexts.name);
+        // extracting texts from spans inside td which contains all info typed above 
+        const info = getTextFromChildElements($tr.find(
+          boardView.tableTdClass).eq(4),'span', [0,1,2,3,4]);
+        expect(info)   
+          .to.include.members([
+            inputsTexts.personAge, 
+            inputsTexts.height,
+            inputsTexts.birthPlace, 
+            inputsTexts.extremlyNested,             
+          ]); 
       });
   }); 
 });
