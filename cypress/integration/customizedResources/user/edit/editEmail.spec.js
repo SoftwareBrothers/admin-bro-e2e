e4330915103c04255bcf374a61719f12ee9c6db7
Adam Wilkanowski <adam.wilkanowski@softwarebrothers.co>
2020-01-13 10:32:12 +0100

    fix

--- cypress/integration/customizedResources/user/edit/editEmail.spec.js
+++ cypress/integration/customizedResources/user/edit/editEmail.spec.js
@@ -17,17 +17,17 @@ describe('Editing email in first record on the list',function(){
   it('Check does changed fields in records are applied on main page', function(){
     cy.loginSuccess() 
       .get(leftNavbar.customized.user).contains(navbarTexts.customized.user).click()
-      .get(boardView.table).find(boardView.tableTr).eq(1).find(boardView.tableTds).eq(0).then($td=>{
+      .get(boardView.table).find(boardView.tableTr).eq(1).find(boardView.tableTds).eq(1).then($td=>{
         cy.wrap($td).as('firstEmail');
       })
-      .get(boardView.tableTds).first().find('a').click()
+      .get(boardView.tableTds).find('a').first().click()
       .get(buttons.edit).click()
       .get(inputs.email).clear().type(editEmail)
       .get(buttons.save).contains(common.save).click()
       .wait(500)
       .get(buttons.back).click()
       .get(boardView.table).find(boardView.tableTr).eq(1).then($tr=>{
-        const editedEmail = getFormValues($tr, [0]);
+        const editedEmail = getFormValues($tr, [1]);
         expect(this.firstEmail.text()).to.deep.not.eq(editedEmail);  
       });
   });  
