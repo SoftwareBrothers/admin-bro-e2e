b52e98f8dc857a3689df24672f225de754e5bc57
Arek <arkadiusz.pecherz@rst-it.com>
2020-03-06 14:21:52 +0100

    - added waiting for create user request

--- cypress/integration/sequelize-resources/users/delete-user.spec.js
+++ cypress/integration/sequelize-resources/users/delete-user.spec.js
@@ -2,16 +2,18 @@ import { common } from '../../../support/texts';
 import comp from '../../../support/components';
 import faker from 'faker';
 
-describe('[Sequelize Resources/ Favourite places] Remove user', function () {
+describe('[Sequelize Resources/ Users] Remove user', function () {
   it('Should enter users and delete created one', function () {
     const userEmail = 'aaaa'+faker.internet.email();
     const userFirstName = faker.name.firstName();
     const userLastName = faker.name.lastName();
 
     cy.server()
-      .route('GET', '/admin/api/resources/Users/records/*/delete').as('recordDeleted');
+      .route('GET', '/admin/api/resources/Users/records/*/delete').as('recordDeleted')
+      .route('POST', '/admin/api/resources/Users/actions/new').as('recordCreated');
 
     cy.addNewUser(userEmail, userLastName, userFirstName)
+      .wait('@recordCreated')
       .get(comp.common.emailsList).contains(userEmail).click()
       .get(comp.common.actionButton).contains('Delete').click()
       .wait('@recordDeleted').its('status').should('eql', 200)
