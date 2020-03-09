d1bdb2aee1124066a1a96498a2f800b7b3ea20f6
Arek <arkadiusz.pecherz@rst-it.com>
2020-03-09 16:21:01 +0100

    - removed redundant 'aaaa' prefix

--- cypress/integration/sequelize-resources/users/add-user.spec.js
+++ cypress/integration/sequelize-resources/users/add-user.spec.js
@@ -5,7 +5,7 @@ import { routeRecordCreated } from '../../../support/route-requests';
 
 describe('[Sequelize Resources/ Users] Add new user to Sequelize Resources', function () {
   it('Should create a new user', function () {
-    const userEmail = 'aaaa'+faker.internet.email();
+    const userEmail = faker.internet.email();
     const userFirstName = faker.name.firstName();
     const userLastName = faker.name.lastName();
 
